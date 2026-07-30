fa87511fc4b575ef615d4814adc0e27e379515ec
Higal Ekombe <higalekombe@gmail.com>
2026-07-31 00:41:02 +0300

    Rearranged the endpoints in the messageRoute

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -4,37 +4,6 @@ import User from "../models/users.js"
 
 const router = express.Router();
 
-router.get("/:chatId",  async (req, res) => {
-   try {
-     const{chatId} = req.params
-    const messages = await Message.find({chatId})
-    .sort({createdAt: 1});
-
-    res.json(messages)
-   }catch(err) {
-    console.error("Failed fetching mesages", err);
-    res.status(500).json({error: "Server error please"})
-   }
-});
-
-router.post("/send", async (req, res) => {
-   try {
-      const {senderId, receiverId, text, type} = req.body;
-      if(!senderId || !receiverId) {
-         return res.status(400).json({message: "There is a missing sender or receiver"})
-      }
-      const chatId = [senderId, receiverId].sort().join("_");
-
-      const message = await Message.create({
-         senderId, receiverId, chatId, text, type: type || "text"
-      });
-      res.json(message);
-
-   }catch(err) {
-      console.error(err)
-      res.status(500).json({message: "Sorry falailed to send message"});
-   }
-});
 
 router.get("/conversations/:userId", async (req, res) => {
    try {
@@ -68,5 +37,38 @@ res.json([...chatMap.values()])
       res.status(500).json({message: "failed to load Converstaions"})
    }
 })
+
+router.get("/:chatId",  async (req, res) => {
+   try {
+     const{chatId} = req.params
+    const messages = await Message.find({chatId})
+    .sort({createdAt: 1});
+
+    res.json(messages)
+   }catch(err) {
+    console.error("Failed fetching mesages", err);
+    res.status(500).json({error: "Server error please"})
+   }
+});
+
+router.post("/send", async (req, res) => {
+   try {
+      const {senderId, receiverId, text, type} = req.body;
+      if(!senderId || !receiverId) {
+         return res.status(400).json({message: "There is a missing sender or receiver"})
+      }
+      const chatId = [senderId, receiverId].sort().join("_");
+
+      const message = await Message.create({
+         senderId, receiverId, chatId, text, type: type || "text"
+      });
+      res.json(message);
+
+   }catch(err) {
+      console.error(err)
+      res.status(500).json({message: "Sorry falailed to send message"});
+   }
+});
+
 
 export default router;
